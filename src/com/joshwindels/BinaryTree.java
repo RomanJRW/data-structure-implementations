93f06e3967dd2e41dd31d6715dc7a5b739131225
Josh.Windels <joshuawindels@gmail.com>
2018-08-15 00:19:22 +0100

    add initial Binary Tree code and insertion method

---
+++ src/com/joshwindels/BinaryTree.java
@@ -0,0 +1,61 @@
+package com.joshwindels;
+
+public class BinaryTree {
+
+    private TreeNode rootNode;
+
+    public BinaryTree() {
+        rootNode =  null;
+    }
+
+    public void addValue(int value) {
+        rootNode = addValueRecursive(rootNode, value);
+    }
+
+    private TreeNode addValueRecursive(TreeNode currentNode, int value) {
+        if (currentNode == null) {
+            return new TreeNode(value);
+        }
+        if (value < currentNode.getValue()){
+            currentNode.setLeftChild(addValueRecursive(currentNode.getLeftChild(), value));
+        } else if (value > currentNode.getValue()) {
+            currentNode.setRightChild(addValueRecursive(currentNode.getRightChild(), value));
+        }
+        return currentNode;
+    }
+
+    private class TreeNode {
+
+        int value;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        public TreeNode(int value) {
+            this.value = value;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public TreeNode getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(TreeNode leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public TreeNode getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(TreeNode rightChild) {
+            this.rightChild = rightChild;
+        }
+
+    }
+
+}
